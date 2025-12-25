098758e44c795955e7f191c8aa9587d450c41449
Molnaw <164439636+Molnaw@users.noreply.github.com>
2025-12-25 12:40:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello from MyTradingBot on Render!"
+    return "Hello from MyTradingBot on Render"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
     
-    
-    
-
